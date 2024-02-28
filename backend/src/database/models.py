3e3e2006c090535fa52cede412ef830353012fb8
young <yahoyoungho@gmail.com>
2024-02-28 00:59:39 -0800

    adding columns to tables

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,10 +1,42 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Float
+from sqlalchemy.orm import mapped_column
 from .database import Base
+from typing import Optional
+
 
 class Restaurant(Base):
     __tablename__ = "restaurants"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
-    location = Column(String)
+    streetAddress = Column(String)
+    city = Column(String)
+    stateProvice = Column(String)
+    postalCode = Column(String)
+    latitude = Column(Float)
+    longitude = Column(Float)
     cuisine_type = Column(String)
+    rating = Column(Float)
+    description = Column(String)
+    websiteURL = Column(String)
+    contactInfo = Column(String)
+
+
+
+class Menus(Base):
+    __tablename__="menus"
+
+    id = Column(Integer, primary_key=True, index=True)
+    restaurant_id = mapped_column(ForeignKey("restaurants.id"))
+    name = Column(String)
+    price = Column(Numeric)
+    imageURL = Column(String)
+    rating = Column(Float)
+
+
+class Users(Base):
+    __tablename__="users"
+    id = Column(Integer, primary_key=True, index=True)
+    userName = Column(String)
+    email = Column(String)
+
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI, Depends
-from . import crud
+import uvicorn
+import crud
 
 app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    # for testing purpose
+    # todo: remove this function when necessary
+    return {"Hello": "World"}
+
 
 @app.get("/restaurants/")
 async def read_restaurants(skip: int = 0, limit: int = 10):
     restaurants = await crud.get_restaurants(skip=skip, limit=limit)
     return restaurants
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="localhost", port=8000, log_level="debug", reload=True)
